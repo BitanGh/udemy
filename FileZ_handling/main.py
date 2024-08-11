b6ba615148fd4940581ce18d175de43dcd49ff24
Bitan Ghosh <bitanghosh10@gmail.com>
2024-08-11 13:18:05 +0530

    Rewriting

--- FileZ_handling/main.py
+++ FileZ_handling/main.py
@@ -1,3 +1,7 @@
-file = open("example.txt","r")
-for line in file:
-    print(line.strip()) #.strip() method helps in removing the empty lines 
+#file = open("example.txt","r")
+#for line in file:
+    #print(line.strip()) #.strip() method helps in removing the empty lines 
+    
+file = open("example.txt","w")
+file.write("This is a new line")
+print("new line added")
