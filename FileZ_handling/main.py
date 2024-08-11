24d47eba1f46ebbbd6ded10244e4186c5fa4b49a
Bitan Ghosh <bitanghosh10@gmail.com>
2024-08-11 14:01:24 +0530

    Assignment 1

--- FileZ_handling/main.py
+++ FileZ_handling/main.py
@@ -12,8 +12,18 @@
 #file.write("This is a new line part-2\n")
 #print("new line added")
 
-file = open("example.txt","w")
-lines = ["First line \n", "Second Line\n", "Third Line\n"]
+#file = open("example.txt","w")
+#lines = ["First line \n", "Second Line\n", "Third Line\n"]
 
-file.writelines(lines)
-print("new line added")
+#file.writelines(lines)
+#print("new line added")
+
+###  Reading information from a source_file and writing the same info. in some other file say Destination.txt
+
+source_file = open('example.txt',"r")
+print(source_file.read())
+content = source_file.read()
+
+destination_file = open('destination.txt',"w")
+destination_file.write(content)
+print("Succesfully Done")
