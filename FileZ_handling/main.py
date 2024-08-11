6f45fc93d27e57e46e1b1d372224b7c065a79b5f
Bitan Ghosh <bitanghosh10@gmail.com>
2024-08-11 13:42:49 +0530

    Adding more lines

--- FileZ_handling/main.py
+++ FileZ_handling/main.py
@@ -2,6 +2,18 @@
 #for line in file:
     #print(line.strip()) #.strip() method helps in removing the empty lines 
     
+#file = open("example.txt","w")
+#file.write("This is a new line")
+#print("new line added")
+
+###File Handling without overwriting
+
+#file = open("example.txt","a")
+#file.write("This is a new line part-2\n")
+#print("new line added")
+
 file = open("example.txt","w")
-file.write("This is a new line")
+lines = ["First line \n", "Second Line\n", "Third Line\n"]
+
+file.writelines(lines)
 print("new line added")
